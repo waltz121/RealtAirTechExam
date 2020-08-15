100043ea643e029fa864f3076258048196d28d50
Walter R. Robledo <robleo.walterrobbin@gmail.com>
2020-08-15 19:29:38 +0800

    Added Condition For Displaying Task Link

---
+++ RealtAirTechExam/Controllers/UserTaskController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealtAirTechExam.Controllers
+{
+    [Authorize]
+    public class UserTaskController : Controller
+    {
+        // GET: UserTask
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
